e9c0a6f9575bdefee545fbe61865a93d563347da
bryanduxbury <bryan@rapleaf.com>
2011-03-23 15:23:48 -0700

    sort out names of test rails projects

---
+++ test/test_project/database_2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Database2::Application.config.secret_token = '145b00790848f29f05cac99bfbc623090add8c894774fd60850e5e0300fa46ff8029a1dfaa28568016420012355deb494d9c23742a7167ab928ae1a2e6025ef4'
