6c432812f52b9370c380525d21206a8601f94419
bryanduxbury <bryan@rapleaf.com>
2011-03-23 15:18:05 -0700

    add initial rails project

---
+++ test/rails/test_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestProject::Application.config.secret_token = 'e9929e7a738b47c80161cea1b69b839547c84402b0b03866dbd44728c64a9e6c0c20c369e0ebd23c4d6eb38d3c563b879fcffe2cc64484a9e4c198c1b38f4c9d'
