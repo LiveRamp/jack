2fafc9aa3cbe7e75ea401eb70b0e68204fdbaa35
bryanduxbury <bryan@rapleaf.com>
2011-04-01 11:13:54 -0700

    factor requires out of jack.rb

--- src/rb/jack.rb
+++ src/rb/jack.rb
@@ -12,20 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'rubygems'
-require 'erb'
-require "fileutils"
-require "rails"
-
-
-require File.expand_path(File.dirname(__FILE__) + "/association_defn")
-require File.expand_path(File.dirname(__FILE__) + "/field_defn")
-require File.expand_path(File.dirname(__FILE__) + "/model_defn")
-require File.expand_path(File.dirname(__FILE__) + "/database_defn")
-require File.expand_path(File.dirname(__FILE__) + "/project_defn")
-require File.expand_path(File.dirname(__FILE__) + "/schema_rb_parser")
-require File.expand_path(File.dirname(__FILE__) + "/models_dir_processor")
-require File.expand_path(File.dirname(__FILE__) + "/template_processor")
+require File.expand_path(File.dirname(__FILE__)) + "/requires.rb"
 
 project_yml = ARGV[0]
 output_dir = ARGV[1]
