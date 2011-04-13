a2509f7625fbe1e2d875b7fa9d0e8ca1d288d202
bryanduxbury <bryan@rapleaf.com>
2011-04-13 14:33:46 -0700

    try to deal with exit codes better in migrate_project.sh

--- test/migrate_project.sh
+++ test/migrate_project.sh
@@ -2,5 +2,15 @@
 pushd `pwd`
 cd $1
 bundle install
+EXIT_CODE=$?
+if [ EXIT_CODE -ne 0 ]
+then
+  exit EXIT_CODE
+fi
 rake db:migrate
+EXIT_CODE=$?
+if [ EXIT_CODE -ne 0 ]
+then
+  exit EXIT_CODE
+fi
 popd
