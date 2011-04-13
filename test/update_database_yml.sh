134abd76cc2f2cc68c9d03ba3c64b22a822bf5c0
bryanduxbury <bryan@rapleaf.com>
2011-04-13 15:02:38 -0700

    oy, stupid regexes

--- test/update_database_yml.sh
+++ test/update_database_yml.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat $1 | sed "s/user(name)*: root/username: $2/" | sed "s/pass(word)*: /password: $3/" > build/database.yml.tmp
+cat $1 | sed "s/username: root/username: $2/" | sed "s/password: /password: $3/" > build/database.yml.tmp
 cp build/database.yml.tmp $1
