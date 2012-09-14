e67440c6706033645e26d95758c2c946001c2e06
Sean Carr <sean@rapleaf.com>
2012-09-14 10:58:16 -0700

    add ORDER BY and LIMIT args to findAll()

--- src/java/com/rapleaf/jack/AbstractDatabaseModel.java
+++ src/java/com/rapleaf/jack/AbstractDatabaseModel.java
@@ -533,8 +533,21 @@ public abstract class AbstractDatabaseModel<T extends ModelWithId> implements
 
   @Override
   public Set<T> findAll(String conditions) throws IOException {
-    PreparedStatement stmt = conn.getPreparedStatement("SELECT * FROM "
-        + getTableName() + " WHERE " + conditions + ";");
+    return findAll(conditions, null, null);
+  }
+
+  @Override
+  public Set<T> findAll(String conditions, String orderBy, Integer limit) throws IOException {
+    StringBuilder sql = new StringBuilder("SELECT * FROM ");
+    sql.append(getTableName()).append(" WHERE ").append(conditions);
+    if (orderBy != null) {
+      sql.append(" ORDER BY ").append(orderBy);
+    }
+    if (limit != null) {
+      sql.append(" LIMIT ").append(limit);
+    }
+    sql.append(";");
+    PreparedStatement stmt = conn.getPreparedStatement(sql.toString());
     ResultSet rs = null;
     try {
       rs = stmt.executeQuery();
