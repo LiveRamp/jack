98d0cc2e79bd7b1761a5e282b2be60abaf349329
Eddie Siegel <eddie@rapleaf.com>
2013-07-03 16:01:12 -0700

    Add comment

--- src/rb/models_dir_processor.rb
+++ src/rb/models_dir_processor.rb
@@ -54,6 +54,7 @@ class ModelsDirProcessor
     if not_matches
       matching_lines = matching_lines.reject { |l| l =~ /^\s*#{not_matches}/ }
     end
+    # ":jack_generate => false" flag makes it easy to tell Jack to ignore associations in cases where you are using rails features that jack doesn't support (e.g. polymorphic associations)
     matching_lines.reject{|l| l =~ /:jack_generate\s*=>\s*false/}.map{|l| AssociationDefn.new(l, model_defn)}
   end
 end
