bbcaa1243fe72ecb56ebf0a50b329d59f1f61af1
Porter Westling <pwestling@liveramp.com>
2013-12-19 19:04:54 -0800

    copyright and whitespace

--- src/java/com/rapleaf/jack/ModelIdWrapper.java
+++ src/java/com/rapleaf/jack/ModelIdWrapper.java
@@ -2,7 +2,8 @@ package com.rapleaf.jack;
 
 import java.io.Serializable;
 
-public interface ModelIdWrapper<T extends ModelIdWrapper> extends Serializable, Comparable<T>{
+public interface ModelIdWrapper<T extends ModelIdWrapper> extends Serializable, Comparable<T> {
 
   public Long getId();
+
 }
