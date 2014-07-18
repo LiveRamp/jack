b5ac2d168e30d46ae4c00d93d304ad718eeca818
Romain Faugeroux <rfaugeroux@liveramp.com>
2014-07-17 17:32:39 -0700

    added query() in persistence interface

--- test/java/com/rapleaf/jack/TestModelQuery.java
+++ test/java/com/rapleaf/jack/TestModelQuery.java
@@ -1,19 +1,22 @@
 package com.rapleaf.jack;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
+import com.rapleaf.jack.test_project.DatabasesImpl;
 import com.rapleaf.jack.test_project.IDatabases;
-import com.rapleaf.jack.test_project.database_1.models.Image;
 import com.rapleaf.jack.test_project.database_1.models.Post;
-import com.rapleaf.jack.test_project.database_1.models.User;
 
 public class TestModelQuery extends TestCase {
 
   private static final DatabaseConnection DATABASE_CONNECTION1 = new DatabaseConnection("database1");
-  private Post postModel;
-  private IDatabases dbs;
-  private Image imageModel;
-  private User userModel;
 
-  
+  @Test
+  public void testQuery() {
+
+    IDatabases dbs = new DatabasesImpl(DATABASE_CONNECTION1);
+    Post post1 = new Post(0, "First Post", 100l, 1, 0l);
+    Post post2 = new Post(1L, "Second Post", 100l, 2, 0l);
+
+  }
 }
