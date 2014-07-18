88d4c522a7372f1eea4ecf9a616bd42771b9c573
Romain Faugeroux <rfaugeroux@liveramp.com>
2014-07-18 16:58:17 -0700

    wrote test case for ModelQuery

--- test/java/com/rapleaf/jack/TestModelQuery.java
+++ test/java/com/rapleaf/jack/TestModelQuery.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.util.Set;
 
 import junit.framework.TestCase;
-import org.junit.Test;
 
 import com.rapleaf.jack.test_project.DatabasesImpl;
 import com.rapleaf.jack.test_project.IDatabases;
-import com.rapleaf.jack.test_project.database_1.models.Post;
+import com.rapleaf.jack.test_project.database_1.iface.IUserPersistence;
+import com.rapleaf.jack.test_project.database_1.models.User;
+
 
 public class TestModelQuery extends TestCase {
 
@@ -17,16 +18,41 @@ public class TestModelQuery extends TestCase {
   public void testQuery() throws IOException {
 
     IDatabases dbs = new DatabasesImpl(DATABASE_CONNECTION1);
-    Post post1 = new Post(0, "First Post", 100l, 1, 0l);
-    Post post2 = new Post(1, "Second Post", 100l, 2, 0l);
-    post1.save();
-    post2.save();
+    dbs.getDatabase1().deleteAll();
+    IUserPersistence users = dbs.getDatabase1().users();
 
-    Set<Post> posts = dbs.getDatabase1().posts().query().title("First Post").find();
+    User userA = users.createDefaultInstance().setHandle("A").setBio("Rapper").setNumPosts(1);
+    User userB = users.createDefaultInstance().setHandle("B").setBio("Rapper").setNumPosts(2);
+    User userC = users.createDefaultInstance().setHandle("C").setBio("Drug dealer").setNumPosts(2);
+    User userD = users.createDefaultInstance().setHandle("D").setBio("Cool dentist").setNumPosts(3);
+    userA.save();
+    userB.save();
+    userC.save();
+    userD.save();
 
-    Post post = posts.iterator().next();
-    System.out.println(post.getId());
+    Set<User> result;
 
-    assertEquals(0, post.getId());
+    // Empty query: should return an empty set
+    result = users.query().find();
+    assertTrue(result.isEmpty());
+
+    // A simple query
+    result = users.query().bio("Cool dentist").find();
+    assertEquals(1, result.size());
+    assertTrue(result.contains(userD));
+
+    // A chained query
+    result = users.query().handle("A").bio("Rapper").num_posts(1).find();
+    assertEquals(1, result.size());
+    assertTrue(result.contains(userA));
+
+    // A chained query
+    result = users.query().bio("Rapper").num_posts(2).find();
+    assertEquals(1, result.size());
+    assertTrue(result.contains(userB));
+
+    // A query with no results
+    result = users.query().num_posts(3).bio("Rapper").find();
+    assertTrue(result.isEmpty());
   }
 }
