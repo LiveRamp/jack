5a1c68e3c186d60ff26d3065df17cb08c026a6ee
Romain Faugeroux <rfaugeroux@liveramp.com>
2014-08-19 14:42:04 -0700

    handle null values in equalTo

--- src/java/com/rapleaf/jack/JackMatchers.java
+++ src/java/com/rapleaf/jack/JackMatchers.java
@@ -15,25 +15,17 @@ import com.rapleaf.jack.query_operators.NotIn;
 
 public class JackMatchers {
 
-  public static <T> IQueryOperator<T> equalToOrNull(T value) {
+  public static <T> IQueryOperator<T> equalTo(T value) {
     if (value == null) {
       return new IsNull<T>();
     }
     return new EqualTo<T>(value);
   }
 
-  public static <T> IQueryOperator<T> notEqualToOrNotNull(T value) {
+  public static <T> IQueryOperator<T> notEqualTo(T value) {
     if (value == null) {
       return new IsNotNull<T>();
     }
-    return new NotEqualTo<T>(value);
-  }
-
-  public static <T> EqualTo<T> equalTo(T value) {
-    return new EqualTo<T>(value);
-  }
-
-  public static <T> NotEqualTo<T> notEqualTo(T value) {
     return new NotEqualTo<T>(value);
   }
 
