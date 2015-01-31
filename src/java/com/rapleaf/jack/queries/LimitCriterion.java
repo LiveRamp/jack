aa2ecd5c697b3406b45ef66881229c464b37035b
LiRen Tu <tuliren.git@outlook.com>
2015-01-30 23:45:39 -0800

    fix format for limit criterion

--- src/java/com/rapleaf/jack/queries/LimitCriterion.java
+++ src/java/com/rapleaf/jack/queries/LimitCriterion.java
@@ -1,29 +1,29 @@
 package com.rapleaf.jack.queries;
     
-  public class LimitCriterion implements QueryCondition {
-    private int offset;
-    private int nResults;
+public class LimitCriterion implements QueryCondition {
+  private int offset;
+  private int nResults;
 
-    public LimitCriterion(int offset, int nResults) {
-      this.offset = offset;
-      this.nResults = nResults;
-    }
-
-    public LimitCriterion(int nResults) {
-      this.offset = 0;
-      this.nResults = nResults;
-    }
-
-    public int getOffset() {
-      return offset;
-    }
-
-    public int getNResults() {
-      return nResults;
-    }
-
-    @Override
-    public String getSqlStatement() {
-      return "LIMIT " + nResults + " OFFSET " + offset;
-    }
+  public LimitCriterion(int offset, int nResults) {
+    this.offset = offset;
+    this.nResults = nResults;
   }
+
+  public LimitCriterion(int nResults) {
+    this.offset = 0;
+    this.nResults = nResults;
+  }
+
+  public int getOffset() {
+    return offset;
+  }
+
+  public int getNResults() {
+    return nResults;
+  }
+
+  @Override
+  public String getSqlStatement() {
+    return "LIMIT " + nResults + " OFFSET " + offset;
+  }
+}
