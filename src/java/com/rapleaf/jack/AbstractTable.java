c7f5fc1b178e858d0566f2695d65e0e0d31159d1
LiRen Tu <tuliren.git@outlook.com>
2015-03-19 23:50:43 -0700

    use strings check

--- src/java/com/rapleaf/jack/AbstractTable.java
+++ src/java/com/rapleaf/jack/AbstractTable.java
@@ -3,6 +3,7 @@ package com.rapleaf.jack;
 import java.util.Set;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 
 public class AbstractTable implements Table {
@@ -11,8 +12,8 @@ public class AbstractTable implements Table {
   protected final Set<Column> allColumns;
 
   protected AbstractTable(String table, String alias) {
-    Preconditions.checkArgument(table != null && !table.isEmpty(), "Table name cannot be null or empty.");
-    Preconditions.checkArgument(alias != null && !alias.isEmpty(), "Table alias cannot be null or empty.");
+    Preconditions.checkArgument(Strings.isNullOrEmpty(table), "Table name cannot be null or empty.");
+    Preconditions.checkArgument(Strings.isNullOrEmpty(alias), "Table alias cannot be null or empty.");
     this.table = table;
     this.alias = alias;
     this.allColumns = Sets.newHashSet();
