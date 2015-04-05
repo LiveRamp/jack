11c2a200d71835dc89abe059eddcc21576eea954
LiRen Tu <tuliren.git@outlook.com>
2015-04-05 11:40:09 -0700

    refactor operator between again

--- src/java/com/rapleaf/jack/queries/where_operators/Between.java
+++ src/java/com/rapleaf/jack/queries/where_operators/Between.java
@@ -7,7 +7,14 @@ import com.rapleaf.jack.queries.Column;
 public class Between<V> extends WhereOperator<V> {
 
   public Between(V value1, V value2) {
-    super("BETWEEN ? AND ?", value1);
+    super("BETWEEN ? AND ?");
+
+    // this operator takes care of its own construction
+    if (!(value1 instanceof Column)) {
+      this.parameters.add(value1);
+    } else {
+      this.sqlStatement = StringUtils.replaceOnce(this.sqlStatement, "?", ((Column)value2).getSqlKeyword());
+    }
 
     if (!(value2 instanceof Column)) {
       this.parameters.add(value2);
