75d39fe0c3cb2b0e6077565c0f305b03a0c0458c
LiRen Tu <tuliren.git@outlook.com>
2015-08-17 18:24:49 -0700

    Add model and attribute classes into table

--- src/java/com/rapleaf/jack/queries/AbstractTable.java
+++ src/java/com/rapleaf/jack/queries/AbstractTable.java
@@ -6,16 +6,23 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 
+import com.rapleaf.jack.AttributesWithId;
+import com.rapleaf.jack.ModelWithId;
+
 public class AbstractTable implements Table {
   protected final String table;
   protected final String alias;
+  protected final Class<? extends AttributesWithId> attributeType;
+  protected final Class<? extends ModelWithId> modelType;
   protected final Set<Column> allColumns;
 
-  protected AbstractTable(String table, String alias) {
+  protected AbstractTable(String table, String alias, Class<? extends AttributesWithId> attributeType, Class<? extends ModelWithId> modelType) {
     Preconditions.checkArgument(!Strings.isNullOrEmpty(table), "Table name cannot be null or empty.");
     Preconditions.checkArgument(!Strings.isNullOrEmpty(alias), "Table alias cannot be null or empty.");
     this.table = table;
     this.alias = alias;
+    this.attributeType = attributeType;
+    this.modelType = modelType;
     this.allColumns = Sets.newHashSet();
   }
 
@@ -27,5 +34,15 @@ public class AbstractTable implements Table {
   @Override
   public String getSqlKeyword() {
     return table + " AS " + alias;
+  }
+
+  @Override
+  public Class<? extends AttributesWithId> getAttributeType() {
+    return attributeType;
+  }
+
+  @Override
+  public Class<? extends ModelWithId> getModelType() {
+    return modelType;
   }
 }
--- src/java/com/rapleaf/jack/queries/Table.java
+++ src/java/com/rapleaf/jack/queries/Table.java
@@ -2,10 +2,17 @@ package com.rapleaf.jack.queries;
 
 import java.util.Set;
 
+import com.rapleaf.jack.AttributesWithId;
+import com.rapleaf.jack.ModelWithId;
+
 public interface Table {
 
   public Set<Column> getAllColumns();
 
   public String getSqlKeyword();
+
+  public Class<? extends AttributesWithId> getAttributeType();
+
+  public Class<? extends ModelWithId> getModelType();
 
 }
