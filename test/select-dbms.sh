7d78907725f8b396a069ad9e19ee05ec4cb8eab0
Roshan George <rgeorge@liveramp.com>
2015-11-12 12:39:30 -0800

    Display useful error message when can't select DB

--- test/select-dbms.sh
+++ test/select-dbms.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ $# -eq 0 ]]; then
+  valid_dbs=$(find "${SRCDIR}/../config" -name database.*\.yml | sed 's/.*database\.//; s/\.yml//; s/config\///')
+  printf "No DB specified. Valid choices are: \n${valid_dbs}\n"
+  printf "Usage: select-dbms.sh <db_type>\n"
+  exit -1
+fi;
+
 for root in "config" "test/test_project/database_1/config" "test/test_project/database_2/config"; do
   echo "Copying $root/database.$1.yml -> $root/database.yml"
   pushd $root > /dev/null
