bb67f46922f50dab73dfff4ea918f042867be3b3
Ben Podgursky <bpodgursky@liveramp.com>
2016-07-11 11:26:35 -0500

    no whitespace

--- test/update_database_yml.sh
+++ test/update_database_yml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat $1 | sed "s/username: root/username: $2/" | sed "s/password: /password: $3/" > build/database.yml.tmp
+cat $1 | sed "s/username: root/username: $2/" | sed "s/password:/password: $3/" > build/database.yml.tmp
 cp build/database.yml.tmp $1
