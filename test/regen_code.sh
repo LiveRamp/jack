bfdc9e3f462f4cc7f861bfbceac6644e31270c20
Chris Mullins <cmullins@liveramp.com>
2016-08-01 18:13:25 -0700

    add -l flag to shell scripts

--- test/regen_code.sh
+++ test/regen_code.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/bash -l
 bundle exec ruby src/rb/jack.rb test/test_project/project.yml test/java
--- test/update_database_yml.sh
+++ test/update_database_yml.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash -l
 cat $1 | sed "s/username:.*/username: $2/" | sed "s/password:.*/password: $3/" > build/database.yml.tmp
 cp build/database.yml.tmp $1
