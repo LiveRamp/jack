99d4b9981b07a1e20d03f10fe3d0a670ec135614
Michel Tricot <michel@liveramp.com>
2016-08-05 22:06:52 -0700

    exclude id

--- src/rb/schema_rb_parser.rb
+++ src/rb/schema_rb_parser.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FORBIDDEN_FIELD_NAMES = ["tbl"]
+FORBIDDEN_FIELD_NAMES = ["tbl", "id"]
 
 class SchemaRbParser
   extend HashRegexHelpers
