fc8d9d1e3b0dbc53d08dbf34a8dc3dec77a0d865
Michel Tricot <michel@liveramp.com>
2016-08-05 18:54:03 -0700

    exclude fields + support redshift syntax

--- src/rb/schema_rb_parser.rb
+++ src/rb/schema_rb_parser.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+FORBIDDEN_FIELD_NAMES = ["tbl"]
+
 class SchemaRbParser
   extend HashRegexHelpers
 
@@ -34,10 +36,12 @@ class SchemaRbParser
         while line =~ /^\s*t\.[a-z]+ / && !file_lines.empty?
           matches = line.match(/^\s*t\.([a-z]+)\s*"([^"]+)",?(.*)$/)
           raise "problem with #{model_defn.table_name}" if !matches
+          field_name = matches[2]
+          next if FORBIDDEN_FIELD_NAMES.include?(field_name)
           field_defn = FieldDefn.new(
-              matches[2], 
-              matches[1].to_sym, 
-              ordinal, 
+              field_name,
+              matches[1].to_sym,
+              ordinal,
               FieldDefn.parse_option_fields(matches[3])
           )
 
