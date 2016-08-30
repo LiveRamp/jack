452c55a26632f49c80ee4d32c7aac342a94671dc
Porter Westling <pwestling@liveramp.com>
2016-08-30 15:57:34 -0700

    scope env vars and props by db

--- src/java/com/rapleaf/jack/DatabaseConnectionConfiguration.java
+++ src/java/com/rapleaf/jack/DatabaseConnectionConfiguration.java
@@ -45,17 +45,36 @@ public class DatabaseConnectionConfiguration {
       db_info = Maps.newHashMap();
     }
 
-    String adapter = load("adapter", db_info, "adapter", "database", "JACK_DB_ADAPTER", "jack.db.adapter", new StringIdentity());
-    String host = load("host", db_info, "host", "database", "JACK_DB_HOST", "jack.db.host", new StringIdentity());
-    String dbName = load("database name", db_info, "database", "database", "JACK_DB_NAME", "jack.db.name", new StringIdentity());
-    Optional<Integer> port = loadOpt(db_info, "port", "JACK_DB_PORT", "jack.db.port", new ToInteger());
-    Optional<Boolean> parallelTesting = loadOpt(env_info, "enable_parallel_tests",
-        "JACK_DB_PARALLEL_TESTS", "jack.db.parallel.test", new ToBoolean());
+    String adapter = load("adapter", db_info, "adapter", "database",
+        envVar("JACK_DB_ADAPTER", dbname_key), prop("jack.db.adapter", dbname_key), new StringIdentity());
 
-    Optional<String> username = loadOpt(db_info, "username", "JACK_DB_USERNAME", "jack.db.username", new StringIdentity());
-    Optional<String> password = loadOpt(db_info, "password", "JACK_DB_PASSWORD", "jack.db.password", new StringIdentity());
+    String host = load("host", db_info, "host", "database",
+        envVar("JACK_DB_HOST", dbname_key), prop("jack.db.host", dbname_key), new StringIdentity());
+
+    String dbName = load("database name", db_info, "database", "database",
+        envVar("JACK_DB_NAME", dbname_key), prop("jack.db.name", dbname_key), new StringIdentity());
+
+    Optional<Integer> port = loadOpt(db_info, "port",
+        envVar("JACK_DB_PORT", dbname_key), prop("jack.db.port", dbname_key), new ToInteger());
+
+    Optional<Boolean> parallelTesting = loadOpt(env_info, "enable_parallel_tests",
+        envVar("JACK_DB_PARALLEL_TESTS", dbname_key), prop("jack.db.parallel.test", dbname_key), new ToBoolean());
+
+    Optional<String> username = loadOpt(db_info, "username",
+        envVar("JACK_DB_USERNAME", dbname_key), prop("jack.db.username", dbname_key), new StringIdentity());
+
+    Optional<String> password = loadOpt(db_info, "password",
+        envVar("JACK_DB_PASSWORD", dbname_key), prop("jack.db.password", dbname_key), new StringIdentity());
 
     return new DatabaseConnectionConfiguration(adapter, host, dbName, port, parallelTesting, username, password);
+  }
+
+  private static String envVar(String baseEnvVar, String dbname_key) {
+    return baseEnvVar + "_" + dbname_key.toUpperCase();
+  }
+
+  private static String prop(String baseProp, String dbname_key) {
+    return baseProp + "." + dbname_key;
   }
 
   private static <T> T load(
@@ -74,7 +93,7 @@ public class DatabaseConnectionConfiguration {
       throw new RuntimeException("Unable to find required configuration " + readableName + ". Please set using one of:\n" +
           "Environment Variable: " + envVar + "\n" +
           "Java System Property: " + javaProp + "\n" +
-          "Entry in config/"+mapYmlFile+".yml: " + mapKey);
+          "Entry in config/" + mapYmlFile + ".yml: " + mapKey);
     }
   }
 
