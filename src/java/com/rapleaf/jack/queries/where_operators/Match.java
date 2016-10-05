d646834d71f46458d49ab9110406cabe2730bb58
Deepak Warrier <deepak.warrier@liveramp.com>
2016-10-05 11:45:23 -0700

    changes for matching columns w/ test

--- src/java/com/rapleaf/jack/queries/where_operators/Match.java
+++ src/java/com/rapleaf/jack/queries/where_operators/Match.java
@@ -1,8 +1,16 @@
 package com.rapleaf.jack.queries.where_operators;
 
+import com.google.common.base.Preconditions;
+import com.rapleaf.jack.queries.Column;
+
 public class Match extends WhereOperator<String> {
 
   public Match(String pattern) {
     super("LIKE ?", pattern);
+  }
+
+  public Match(Column<String> column, String prefix, String suffix) {
+    super(String.format("LIKE CONCAT('%s', %s, '%s')", prefix, column.getSqlKeyword(), suffix));
+    Preconditions.checkNotNull(column);
   }
 }
