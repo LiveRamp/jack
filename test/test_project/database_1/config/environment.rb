556a60232b07debea2d1106246483c2ed507990c
LiRen Tu <tuliren.git@outlook.com>
2016-11-07 13:59:32 -0800

    Add patches to fix default null key issue in mysql 5.7 (thanks to @sun16)

--- test/test_project/database_1/config/environment.rb
+++ test/test_project/database_1/config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TestProject::Application.initialize!
+
+# This can be removed once this test project is upgraded to rails 4
+require File.expand_path('../../lib/patches/57key.rb', __FILE__)
