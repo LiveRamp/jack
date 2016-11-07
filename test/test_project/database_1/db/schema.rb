5e90ad36f3e25e120c372a102dc7edeb24bc1991
LiRen Tu <tuliren.git@outlook.com>
2016-11-07 14:40:31 -0800

    Use rails 4

--- test/test_project/database_1/db/schema.rb
+++ test/test_project/database_1/db/schema.rb
@@ -9,45 +9,45 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151226013740) do
+ActiveRecord::Schema.define(version: 20151226013740) do
 
-  create_table "comments", :force => true do |t|
-    t.text     "content"
-    t.integer  "commenter_id",                                                    :null => false
-    t.integer  "commented_on_id", :limit => 8,                                    :null => false
-    t.datetime "created_at",                   :default => '1970-01-01 00:00:00', :null => false
+  create_table "comments", force: :cascade do |t|
+    t.text     "content",         limit: 65535
+    t.integer  "commenter_id",    limit: 4,                                     null: false
+    t.integer  "commented_on_id", limit: 8,                                     null: false
+    t.datetime "created_at",                    default: '1970-01-01 00:00:00', null: false
   end
 
-  create_table "images", :force => true do |t|
-    t.integer "user_id"
+  create_table "images", force: :cascade do |t|
+    t.integer "user_id", limit: 4
   end
 
-  create_table "lockable_models", :force => true do |t|
-    t.integer  "lock_version", :default => 0, :null => false
-    t.text     "message"
-    t.datetime "created_at",                  :null => false
-    t.datetime "updated_at",                  :null => false
-  end
-
-  create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.date     "posted_at_millis"
-    t.integer  "user_id"
+  create_table "lockable_models", force: :cascade do |t|
+    t.integer  "lock_version", limit: 4,     default: 0, null: false
+    t.text     "message",      limit: 65535
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "handle",                                                         :null => false
-    t.integer  "created_at_millis", :limit => 8
-    t.integer  "num_posts",                                                      :null => false
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",            limit: 255
+    t.date     "posted_at_millis"
+    t.integer  "user_id",          limit: 4
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "handle",            limit: 255,                             null: false
+    t.integer  "created_at_millis", limit: 8
+    t.integer  "num_posts",         limit: 4,                               null: false
     t.date     "some_date"
     t.datetime "some_datetime"
-    t.text     "bio"
-    t.binary   "some_binary"
-    t.float    "some_float"
-    t.decimal  "some_decimal",                   :precision => 20, :scale => 10
+    t.text     "bio",               limit: 65535
+    t.binary   "some_binary",       limit: 65535
+    t.float    "some_float",        limit: 24
+    t.decimal  "some_decimal",                    precision: 20, scale: 10
     t.boolean  "some_boolean"
   end
 
