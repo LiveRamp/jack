a718ec4465fd2af2360e094a8d301ff9873b8b5f
LiRen Tu <tuliren.git@outlook.com>
2016-11-07 14:37:27 -0800

    Remove patch (will try to upgrade to rails 4 instead)

--- test/test_project/database_2/config/environment.rb
+++ test/test_project/database_2/config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Database2::Application.initialize!
-
-# This can be removed once this test project is upgraded to rails 4
-require File.expand_path('../../lib/patches/57key.rb', __FILE__)
--- test/test_project/database_2/lib/patches/57key.rb
+++
@@ -1,4 +0,0 @@
-# This can be removed once this test project is upgraded to rails 4
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-end
