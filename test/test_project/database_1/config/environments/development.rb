1967ee3022e2f8c6914eebaef8816d012df0c8b5
LiRen Tu <tuliren.git@outlook.com>
2016-11-07 14:39:10 -0800

    Get ready for rails 4

--- test/test_project/database_1/config/environments/development.rb
+++ test/test_project/database_1/config/environments/development.rb
@@ -11,7 +11,7 @@ TestProject::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  # config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,5 +22,6 @@ TestProject::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-end
 
+  config.eager_load = false
+end
