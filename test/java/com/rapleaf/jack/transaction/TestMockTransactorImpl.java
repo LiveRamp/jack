5679ff6ecc2c093930a769e713fcb9b227a9078e
LiRen Tu <tuliren.git@outlook.com>
2017-02-25 00:58:03 -0800

    Add mock test for exception

--- test/java/com/rapleaf/jack/transaction/TestMockTransactorImpl.java
+++ test/java/com/rapleaf/jack/transaction/TestMockTransactorImpl.java
@@ -1,13 +1,17 @@
 package com.rapleaf.jack.transaction;
 
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 
 import com.rapleaf.jack.IDb;
+import com.rapleaf.jack.exception.SqlExecutionFailureException;
 import com.rapleaf.jack.test_project.database_1.IDatabase1;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
@@ -33,7 +37,7 @@ public class TestMockTransactorImpl {
   @Test
   public void testMockExecution() throws Exception {
     transactor.execute(IDb::deleteAll);
-    verifyExecuteMethod(false);
+    verifyExecuteMethod(false, false);
   }
 
   @Test
@@ -41,32 +45,90 @@ public class TestMockTransactorImpl {
   public void testMockQuery() throws Exception {
     boolean deleteAll = transactor.execute(IDb::deleteAll);
     assertTrue(deleteAll);
-    verifyExecuteMethod(false);
+    verifyExecuteMethod(false, false);
   }
 
   @Test
   public void testMockExecutionAsTransaction() throws Exception {
     transactor.executeAsTransaction(IDb::deleteAll);
-    verifyExecuteMethod(true);
+    verifyExecuteMethod(true, false);
   }
 
   @Test
   public void testMockQueryAsTransaction() throws Exception {
     boolean deleteAll = transactor.executeAsTransaction(IDb::deleteAll);
     assertTrue(deleteAll);
-    verifyExecuteMethod(true);
+    verifyExecuteMethod(true, false);
   }
 
-  private void verifyExecuteMethod(boolean asTransaction) throws Exception {
+  @Test
+  public void testMockExecutionWithException() throws Exception {
+    try {
+      transactor.execute((IExecution<IDatabase1>)db -> {
+        db.deleteAll();
+        throw new IOException();
+      });
+      fail();
+    } catch (SqlExecutionFailureException e) {
+      verifyExecuteMethod(false, true);
+    }
+  }
+
+  @Test
+  public void testMockQueryWithException() throws Exception {
+    try {
+      boolean result = transactor.execute((IQuery<IDatabase1, Boolean>)db -> {
+        db.deleteAll();
+        throw new IOException();
+      });
+      fail();
+    } catch (SqlExecutionFailureException e) {
+      verifyExecuteMethod(false, true);
+    }
+  }
+
+  @Test
+  public void testMockExecutionAsTransactionWithException() throws Exception {
+    try {
+      transactor.executeAsTransaction((IExecution<IDatabase1>)db -> {
+        db.deleteAll();
+        throw new IOException();
+      });
+      fail();
+    } catch (SqlExecutionFailureException e) {
+      verifyExecuteMethod(true, true);
+    }
+  }
+
+  @Test
+  public void testMockQueryAsTransactionWithException() throws Exception {
+    try {
+      boolean result = transactor.executeAsTransaction((IQuery<IDatabase1, Boolean>)db -> {
+        db.deleteAll();
+        throw new IOException();
+      });
+      fail();
+    } catch (SqlExecutionFailureException e) {
+      verifyExecuteMethod(true, true);
+    }
+  }
+
+  private void verifyExecuteMethod(boolean asTransaction, boolean withException) throws Exception {
     boolean autoCommit = !asTransaction;
 
     InOrder dbMethodOrder = inOrder(dbManager, db);
     dbMethodOrder.verify(dbManager, times(1)).getConnection(anyLong());
     dbMethodOrder.verify(db, times(1)).setAutoCommit(autoCommit);
     dbMethodOrder.verify(db, times(1)).deleteAll();
+
     if (asTransaction) {
-      dbMethodOrder.verify(db, times(1)).commit();
+      if (withException) {
+        dbMethodOrder.verify(db, times(1)).rollback();
+      } else {
+        dbMethodOrder.verify(db, times(1)).commit();
+      }
     }
+
     dbMethodOrder.verify(dbManager, times(1)).returnConnection(db);
   }
 
