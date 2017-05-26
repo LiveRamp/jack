fd8cbe63b932134698c7eaccb216d1686e17f3be
Joel Croteau <jcroteau@liveramp.com>
2017-05-25 18:53:56 -0700

    Update generated code

--- test/java/com/rapleaf/jack/test_project/database_1/impl/Database1Impl.java
+++ test/java/com/rapleaf/jack/test_project/database_1/impl/Database1Impl.java
@@ -93,15 +93,11 @@ public class Database1Impl implements IDatabase1 {
 
   public boolean deleteAll() throws IOException {
     boolean success = true;
-    try {
     success &= comments().isEmpty() || comments().deleteAll();
     success &= images().isEmpty() || images().deleteAll();
     success &= lockableModels().isEmpty() || lockableModels().deleteAll();
     success &= posts().isEmpty() || posts().deleteAll();
     success &= users().isEmpty() || users().deleteAll();
-    } catch (IOException e) {
-      throw e;
-    }
     return success;
   }
 
