ff4c17fdbf9b01300c7dfc35f5841f512ea92685
Liren Tu <ltu@liveramp.com>
2017-06-25 21:13:08 -0700

    Split test tuple paths into two separate files

--- jack-test/test/java/com/rapleaf/jack/store/json/TestJsonDbHelper.java
+++ jack-test/test/java/com/rapleaf/jack/store/json/TestJsonDbHelper.java
@@ -9,47 +9,7 @@ import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestJsonDbHelper {
-
-  public static final String COMPLEX_JSON_STRING = "{" +
-      "key1: val1," +
-      "key2 :" +
-      "  {" +
-      "    array1: [1, 2, 3]," +
-      "    array2: [four, five]," +
-      "    key3: val2," +
-      "    deepObject:" +
-      "       {" +
-      "         deepKey: 0.54," +
-      "         deepArray: [false, true, false]," +
-      "         emptyObject: {}" +
-      "       }," +
-      "    deepArray: [21, 22, 23]" +
-      "  }," +
-      "arraysInArray :" +
-      "  [" +
-      "    [1, 2, 3]," +
-      "    [4, 5, 6]," +
-      "    [7, 8, 9]" +
-      "  ]," +
-      "objectsInArray :" +
-      "  [" +
-      "    {" +
-      "      k1: [true, false, true]," +
-      "      k2: 11" +
-      "    }," +
-      "    {" +
-      "      k1: [true, false, true]," +
-      "      k2: 12," +
-      "      emptyArray: []" +
-      "    }" +
-      "  ]," +
-      "array3: [6,7,8,9,10]," +
-      "bool1: true," +
-      "bool2: false," +
-      "nullKey: null," +
-      "nullStrKey: \"null\"" +
-      "}";
+public class TestJsonDbHelper extends BaseJsonTestCase {
 
   private final JsonParser parser = new JsonParser();
   private String jsonString;
@@ -199,4 +159,5 @@ public class TestJsonDbHelper {
     jsonString = COMPLEX_JSON_STRING;
     testJson();
   }
+
 }
