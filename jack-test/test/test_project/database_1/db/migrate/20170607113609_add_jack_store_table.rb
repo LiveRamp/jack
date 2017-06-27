2bf7b6c01bd9ee6e2566bab91bbb67cbc087bd50
Liren Tu <ltu@liveramp.com>
2017-06-27 14:52:06 -0700

    Change scope column from string to long

--- jack-test/test/test_project/database_1/db/migrate/20170607113609_add_jack_store_table.rb
+++ jack-test/test/test_project/database_1/db/migrate/20170607113609_add_jack_store_table.rb
@@ -1,7 +1,7 @@
 class AddJackStoreTable < ActiveRecord::Migration
   def change
     create_table :test_store do |t|
-      t.string :scope
+      t.integer :scope, :limit => 8
       t.string :key
       t.string :type
       t.string :value
@@ -9,7 +9,7 @@ class AddJackStoreTable < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :test_store, [:scope, :key], :name => 'store_index_on_scope_key'
-    add_index :test_store, [:scope, :value], :name => 'store_index_on_scope_value'
+    add_index :test_store, [:scope, :key], :name => 'store_index_on_scope_key', :length => {:key => 100}
+    add_index :test_store, [:scope, :value], :name => 'store_index_on_scope_value', :length => {:value => 100}
   end
 end
