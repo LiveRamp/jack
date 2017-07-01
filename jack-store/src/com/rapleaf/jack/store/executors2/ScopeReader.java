e9560a86f64b314a581a86d4025efcb8ef206149
Liren Tu <ltu@liveramp.com>
2017-07-01 16:42:57 -0700

    Implement scope reader

--- jack-store/src/com/rapleaf/jack/store/executors2/ScopeReader.java
+++ jack-store/src/com/rapleaf/jack/store/executors2/ScopeReader.java
@@ -1,20 +1,53 @@
 package com.rapleaf.jack.store.executors2;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import com.rapleaf.jack.IDb;
+import com.rapleaf.jack.queries.Record;
+import com.rapleaf.jack.queries.Records;
 import com.rapleaf.jack.store.JsRecord;
 import com.rapleaf.jack.store.JsTable;
+import com.rapleaf.jack.store.ValueType;
+import com.rapleaf.jack.store.json.JsonDbTuple;
 
-public class ScopeReader extends BaseExecutor2<JsRecord> {
+public class ScopeReader extends BaseReaderExecutor2<ScopeReader> {
 
-  public ScopeReader(JsTable table, Long executionScopeId) {
+  ScopeReader(JsTable table, Long executionScopeId) {
     super(table, executionScopeId);
   }
 
   @Override
+  ScopeReader getSelf() {
+    return this;
+  }
+
+  @Override
   public JsRecord execute(IDb db) throws IOException {
-    return null;
+    Records records = db.createQuery().from(table.table)
+        .where(table.scopeColumn.equalTo(executionScopeId))
+        .where(table.typeColumn.notEqualTo(ValueType.SCOPE.value))
+        .select(table.typeColumn, table.keyColumn, table.valueColumn)
+        .orderBy(table.idColumn)
+        .fetch();
+
+    Map<String, ValueType> types = Maps.newHashMap();
+    Map<String, Object> values = Maps.newHashMap();
+    List<JsonDbTuple> jsonTuples = Lists.newLinkedList();
+    Set<String> jsonKeys = Sets.newHashSet();
+
+    for (Record record : records) {
+      appendRecord(types, values, jsonTuples, jsonKeys, record);
+    }
+    appendJsonRecord(types, values, jsonTuples, jsonKeys);
+
+    return new JsRecord(executionScopeId, types, values);
   }
 
 }
