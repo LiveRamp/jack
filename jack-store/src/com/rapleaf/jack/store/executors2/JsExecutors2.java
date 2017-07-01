0bd42e8c06ed173cb9743b0a88466350aacefa35
Liren Tu <ltu@liveramp.com>
2017-07-01 01:55:50 -0700

    Update comments in js executors 2

--- jack-store/src/com/rapleaf/jack/store/executors2/JsExecutors2.java
+++ jack-store/src/com/rapleaf/jack/store/executors2/JsExecutors2.java
@@ -1,18 +1,9 @@
 package com.rapleaf.jack.store.executors2;
 
+import com.rapleaf.jack.store.JsRecord;
+import com.rapleaf.jack.store.JsRecords;
 import com.rapleaf.jack.store.JsTable;
 
-/**
- * jackStore.scope().create() -> create scope and add records
- * jackStore.scope().get()    -> get all records under scope
- * jackStore.scope().update() -> rename scope, update records under scope
- * jackStore.scope().delete() -> delete scope and records under scope
- * <p>
- * jackStore.scope().createSubScope() -> new subscope under scope and records under new subscope
- * jackStore.scope().querySubScopes() -> records in subscopes under scope
- * jackStore.scope().updateSubscopes() -> update records under subscopes under scope
- * jackStore.scope().deleteSubscopes() -> delete records under subscopes under scope
- */
 public class JsExecutors2 {
 
   private final JsTable table;
@@ -23,30 +14,69 @@ public class JsExecutors2 {
     this.executionScopeId = executionScopeId;
   }
 
+  /**
+   * Query key value pairs under the current scope.
+   *
+   * @return {@link JsRecord}
+   */
   public ScopeReader query() {
     return new ScopeReader(table, executionScopeId);
   }
 
+  /**
+   * Insert or update key value pairs under the current scope.
+   *
+   * @return {@link JsRecord}
+   */
   public ScopeUpdater update() {
     return new ScopeUpdater(table, executionScopeId);
   }
 
+  /**
+   * Delete key value pairs under the current scope.
+   * Delete the current scope.
+   *
+   * @return {@link Void}
+   */
   public ScopeDeleter delete() {
     return new ScopeDeleter(table, executionScopeId);
   }
 
+  /**
+   * Create a new scope under the current scope.
+   * Insert key value pairs under the new scope.
+   *
+   * @return {@link JsRecord}
+   */
   public SubScopeCreator createSubScope() {
     return new SubScopeCreator(table, executionScopeId);
   }
 
+  /**
+   * Query the sub scopes under the current scope.
+   * The result set does not include key value pairs directly under the current scope.
+   *
+   * @return {@link JsRecords} representing the fetched sub scopes
+   */
   public SubScopeReader querySubScopes() {
     return new SubScopeReader(table, executionScopeId);
   }
 
+  /**
+   * Insert or update key value pairs in the sub scopes under the current scope.
+   * Key value pairs directly under the current scope will not be affected.
+   *
+   * @return {@link JsRecords} representing the updated sub scopes
+   */
   public SubScopeUpdater updateSubScopes() {
     return new SubScopeUpdater(table, executionScopeId);
   }
 
+  /**
+   * Delete sub scopes under the current scope.
+   *
+   * @return {@link Void}
+   */
   public SubScopeDeleter deleteSubScopes() {
     return new SubScopeDeleter(table, executionScopeId);
   }
