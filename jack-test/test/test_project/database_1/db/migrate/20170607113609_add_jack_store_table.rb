5bea677e5d13940d08c0587b9bb6a254b477461a
Liren Tu <ltu@liveramp.com>
2017-07-11 17:08:04 -0700

    Rename jack store columns to avoid rails reserved keywords

--- jack-test/test/test_project/database_1/db/migrate/20170607113609_add_jack_store_table.rb
+++ jack-test/test/test_project/database_1/db/migrate/20170607113609_add_jack_store_table.rb
@@ -1,14 +1,14 @@
 class AddJackStoreTable < ActiveRecord::Migration
   def change
     create_table :test_store do |t|
-      t.integer :type,  :limit => 4
-      t.integer :scope, :limit => 8
-      t.string :key
-      t.string :value
+      t.integer :entry_type,  :limit => 4
+      t.integer :entry_scope, :limit => 8
+      t.text    :entry_key
+      t.text    :entry_value
 
       t.timestamps
     end
 
-    add_index :test_store, [:scope, :key, :value], :name => 'store_index_on_scope_key_value', :length => {:key => 20, :value => 60}
+    add_index :test_store, [:entry_scope, :entry_key, :entry_value], :name => 'store_index_on_scope_key_value', :length => {:entry_key => 20, :entry_value => 60}
   end
 end
