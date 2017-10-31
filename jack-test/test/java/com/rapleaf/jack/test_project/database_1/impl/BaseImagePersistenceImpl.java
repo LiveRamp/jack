16cd0299e28f021064ea8dcfcb15fdcb08888878
Liren Tu <ltu@liveramp.com>
2017-10-30 22:26:31 -0700

    Add import of JackUtility

--- jack-test/test/java/com/rapleaf/jack/test_project/database_1/impl/BaseImagePersistenceImpl.java
+++ jack-test/test/java/com/rapleaf/jack/test_project/database_1/impl/BaseImagePersistenceImpl.java
@@ -25,6 +25,7 @@ import com.rapleaf.jack.AbstractDatabaseModel;
 import com.rapleaf.jack.BaseDatabaseConnection;
 import com.rapleaf.jack.queries.WhereConstraint;
 import com.rapleaf.jack.queries.WhereClause;
+import com.rapleaf.jack.util.JackUtility;
 import com.rapleaf.jack.test_project.database_1.iface.IImagePersistence;
 import com.rapleaf.jack.test_project.database_1.models.Image;
 import com.rapleaf.jack.test_project.database_1.query.ImageQueryBuilder;
