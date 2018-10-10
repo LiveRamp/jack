55fc206621c233f7a1e83662373ce71be256ad5b
Joshua Kwan <joshua.kwan@liveramp.com>
2018-10-10 17:47:05 -0400

    more followups to #231

--- jack-test/test/create_databases.sh
+++ jack-test/test/create_databases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 
-pushd test/test_project/database_1 && bundle install && bundle exec rake db:drop db:create db:migrate; popd
-pushd test/test_project/database_2 && bundle install && bundle exec rake db:drop db:create db:migrate; popd
+pushd test/test_project/database_1 && bundle install --path vendor/bundle && bundle exec rake db:drop db:create db:migrate; popd
+pushd test/test_project/database_2 && bundle install --path vendor/bundle && bundle exec rake db:drop db:create db:migrate; popd
