47a2d5ba2939485122f94c04ff2c2bb15e8a68da
Henry Zhao <henry.zhao@liveramp.com>
2019-03-04 17:05:29 -0800

    Add comment

--- jack-core/src/com/rapleaf/jack/transaction/DbPoolStatusImpl.java
+++ jack-core/src/com/rapleaf/jack/transaction/DbPoolStatusImpl.java
@@ -10,16 +10,25 @@ public class DbPoolStatusImpl<DB extends IDb> implements DbPoolStatus {
     this.connectionPool = connectionPool;
   }
 
+  /**
+   * @return The number of db connection object currently borrowed from this pool
+   */
   @Override
   public int getNumActive() {
     return this.connectionPool.getNumActive();
   }
 
+  /**
+   * @return The number of db connection object currently idle in this pool
+   */
   @Override
   public int getNumIdle() {
     return this.connectionPool.getNumIdle();
   }
 
+  /**
+   * @return The number of threads currently blocked waiting for an db connection object from the pool
+   */
   @Override
   public int getNumWaiters() {
     return this.connectionPool.getNumActive();
