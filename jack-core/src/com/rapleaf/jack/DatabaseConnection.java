fef096133fb354db2cfb39daa9c6b9c8455d3df0
Oliver Zhang <ziqi.zhang@liveramp.com>
2019-07-22 14:55:17 -0700

    store exception for the final runtimeException

--- jack-core/src/com/rapleaf/jack/DatabaseConnection.java
+++ jack-core/src/com/rapleaf/jack/DatabaseConnection.java
@@ -108,6 +108,7 @@ public class DatabaseConnection extends BaseDatabaseConnection {
    */
 
   public Connection getConnectionInternal() {
+    Exception runtimeException = new Exception();
     for (int retryCount = 0; retryCount < GET_CONNECTION_MAX_RETRIES; ++retryCount) {
       try {
         if (conn == null) {
@@ -124,16 +125,17 @@ public class DatabaseConnection extends BaseDatabaseConnection {
           throw new RuntimeException(String.format("Could not establish connection after %d attempts",
               GET_CONNECTION_MAX_RETRIES), e);
         }
+        runtimeException = e;
         // Wait (2^retryCount) seconds
         try {
           Thread.sleep(Math.round(Math.pow(2, retryCount)) * 1000);
         } catch (Exception ignored) {
-          // Catches interruptions in the sleep
+          // ignore InterruptedException during sleep
         }
       }
     }
     throw new RuntimeException(String.format("Could not establish connection after %d attempts",
-        GET_CONNECTION_MAX_RETRIES));
+        GET_CONNECTION_MAX_RETRIES), runtimeException);
   }
 
 
